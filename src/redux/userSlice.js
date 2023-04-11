2395f49ae2a5af6cbbc0b6ebd9da51d24bc0697a
Muhammad Saeed <MuhammadSaeed0336>
2023-04-11 15:08:02 +0500

    redux Toolkit

---
+++ src/redux/userSlice.js
@@ -0,0 +1,36 @@
+import {createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const updateUser2 = createAsyncThunk("users/update", async (user)=>{
+    const res = await axios.post("http://localhost:8081/api/users/123/update", user);
+    return res.data;
+})
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    userInfo: {
+      name: "Muhammad Saeed",
+      email: "abc@gmail.com",
+    },
+    pending: null,
+    error: false,
+  },
+  reducers: {},
+  extraReducers:{
+    [updateUser2.pending]: (state) =>{
+        state.pending =true;
+        state.error = false;
+    },
+    [updateUser2.fulfilled]: (state, action) =>{
+        state.pending =false;
+        state.userInfo = action.payload;
+    },
+    [updateUser2.rejected]: (state) =>{
+        state.pending =null;
+        state.error = true;
+    },
+  }
+});
+
+export const { updateStart, updateSuccess, updateError } = userSlice.actions;
+export default userSlice.reducer;
